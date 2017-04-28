121c2c7f09fcd2245e9e46d235e4e89d1306eb58
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-04-28 13:19:24 +0200

    Update community build test.
    - Automatically infer the test command from .travis.yml
    - Automatically infer dotty version from sbt-dotty
    - No longer use outdated COMPILERVERSION.

---
+++ dotty-community-build/project/plugins.sbt
@@ -0,0 +1,2 @@
+addSbtPlugin("ch.epfl.lamp" % "sbt-dotty" % "0.1.0-RC4")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.7.0")
--- dotty-community-build/src/test/scala/dotty/communitybuild/CommunityProjectTest.scala
+++ dotty-community-build/src/test/scala/dotty/communitybuild/CommunityProjectTest.scala
@@ -1,60 +1,40 @@
 package dotty.communitybuild
 
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
 import org.junit.Assert._
 import org.junit.Test
 
-case class CommunityProject(
-    name: String,
-    sbtCommands: List[String] = List("clean", "compile")
-) {
+case class CommunityProject(name: String) {
   def workingDirectory: Path =
     Paths.get(sys.props("user.dir")).getParent.resolve(name)
 }
 
-object DottyVersion {
-  // NOTE: the "latest" version in the maven-metadata.xml points to a release from
-  // January because that version has no PATCH (0.1-201701XXX) while the actual latest
-  // releases have a PATCH version (0.1.1-2017XXXX). Until 0.2 is released,
-  // we sort by the date (2017XXXX) part and take the latest one by lexicographical order.
-  case class NightlyVersion(monthDay: String, version: String)
-  private val MonthDay = """.*2017(\d\d\d\d).*""".r
-  private val Version = """      <version>(.*)</version>""".r
-  def latestDottyNightlyRelease: Option[String] =
-    scala.io.Source
-      .fromURL(
-        "http://repo1.maven.org/maven2/ch/epfl/lamp/dotty_2.11/maven-metadata.xml")
-      .getLines()
-      .collect {
-        case Version(version @ MonthDay(monthDay)) =>
-          NightlyVersion(monthDay, version)
-      }
-      .toSeq
-      .sortBy(_.monthDay)
-      .lastOption
-      .map(_.version)
-  lazy val latest: String = sys.env.getOrElse(
-    "DOTTY_VERSION",
-    latestDottyNightlyRelease.getOrElse {
-      throw new IllegalStateException(
-        "Unable to automatically fetch latest dotty nightly release, " +
-          "please manually pass the environment variable DOTTY_VERSION.")
-    }
-  )
-}
-
 abstract class CommunityProjectTest(project: CommunityProject) {
-  def log(msg: String) = {
+  private def log(msg: String) = {
     val banner = "*" * (msg.length + 5)
     println(banner)
     println("===> " + msg)
     println(banner)
   }
-  log(s"Using dotty version ${DottyVersion.latest}")
+  log(s"Using dotty version ${BuildInfo.dottyVersion}")
+  private val SbtCommand = ".*sbt ([^ ]+) .*".r
   @Test def compilesWithDotty(): Unit = {
     log(s"Starting....")
+    // By convention, run the first command passed to sbt in .travis.yml
+    val sbtCommand = new String(Files.readAllBytes(
+      project.workingDirectory.resolve(".travis.yml"))).lines
+      .sliding(2)
+      .collectFirst {
+        case "script:" :: SbtCommand(cmd) :: Nil => cmd
+      }
+      .getOrElse {
+        throw new IllegalStateException(
+          s"Unable to infer sbt command to test project $project")
+      }
+
     def exec(binary: String, arguments: String*): Unit = {
       val command = binary +: arguments
       log(s"Running command: ${command.mkString(" ")}")
@@ -63,9 +43,6 @@ abstract class CommunityProjectTest(project: CommunityProject) {
       val builder = new ProcessBuilder(command.asJava)
         .directory(project.workingDirectory.toFile)
         .inheritIO()
-      builder
-        .environment()
-        .put("COMPILERVERSION", DottyVersion.latest)
 
       val process = builder.start()
       val exit = process.waitFor()
@@ -74,7 +51,7 @@ abstract class CommunityProjectTest(project: CommunityProject) {
     exec("git", "clean", "-xfd")
     exec("git", "checkout", "dotty")
     exec("git", "pull", "origin", "dotty")
-    exec("sbt", project.sbtCommands: _*)
+    exec("sbt", s"++${BuildInfo.dottyVersion}", sbtCommand)
     log(s"DONE!")
   }
 }
@@ -83,4 +60,4 @@ class Scalatest extends CommunityProjectTest(CommunityProject("scalatest"))
 
 class Squants extends CommunityProjectTest(CommunityProject("squants"))
 
-class Algebra extends CommunityProjectTest(CommunityProject("algebra", List("coreJVM/compile")))
+class Algebra extends CommunityProjectTest(CommunityProject("algebra"))
