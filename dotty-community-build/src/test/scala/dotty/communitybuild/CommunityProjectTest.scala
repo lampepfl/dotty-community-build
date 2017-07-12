f54751836d697bd79f169475777001689f2060bb
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-07-12 13:35:35 +0200

    Allow no space after inferred sbt command in .travis.yml
    ScalaPB's .travis.yml had a line like this:
    
    ```
      - sbt dottyCompile
    ```
    The regex previously assumed a space would follow the command, causing
    it to fail.

--- dotty-community-build/src/test/scala/dotty/communitybuild/CommunityProjectTest.scala
+++ dotty-community-build/src/test/scala/dotty/communitybuild/CommunityProjectTest.scala
@@ -42,7 +42,7 @@ abstract class CommunityProjectTest(project: CommunityProject) {
   }
   log(s"Using dotty version ${BuildInfo.dottyVersion}")
   assertVersionIsUpToDate(BuildInfo.dottyVersion)
-  private val SbtCommand = ".*sbt ([^ ]+) .*".r
+  private val SbtCommand = ".*sbt ([^ ]+).*".r
   @Test def compilesWithDotty(): Unit = {
     log(s"Starting....")
     // By convention, run the first command passed to sbt in .travis.yml
