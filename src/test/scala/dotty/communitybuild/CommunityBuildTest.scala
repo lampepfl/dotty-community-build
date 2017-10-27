77b6ab05c737516d92d20bf0b4da86ed05366dfe
Allan Renucci <allan.renucci@gmail.com>
2017-10-27 11:20:56 +0200

    Simplify Dotty Community Build
    We get rid of git submodules and use git urls instead

---
+++ src/test/scala/dotty/communitybuild/CommunityBuildTest.scala
@@ -0,0 +1,45 @@
+package dotty.communitybuild
+
+import java.nio.file.{Path, Paths}
+
+import org.junit.{Ignore, Test}
+
+class CommunityBuildTest extends BuildTest {
+  def testDirectory: Path =
+    Paths.get(sys.props("user.dir") + "/out/")
+
+  @Test def algebra = project(
+    gitUrl = "https://github.com/dotty-staging/algebra",
+    command = "coreJVM/compile"
+  )
+
+  @Test def betterfiles = project(
+    gitUrl = "https://github.com/dotty-staging/better-files",
+    command = "dottyCompile"
+  )
+
+  @Test def scalacheck = project(
+    gitUrl = "https://github.com/dotty-staging/scalacheck",
+    command = "jvm/compile"
+  )
+
+  @Test def ScalaPB = project(
+    gitUrl = "https://github.com/dotty-staging/ScalaPB",
+    command = "dottyCompile"
+  )
+
+  @Test def scalatest = project(
+    gitUrl = "https://github.com/dotty-staging/scalatest",
+    command = "scalatest/compile"
+  )
+
+  @Test def scopt = project(
+    gitUrl = "https://github.com/dotty-staging/scopt",
+    command = "scoptJVM/compile"
+  )
+
+  @Test def squants = project(
+    gitUrl = "https://github.com/dotty-staging/squants",
+    command = "squantsJVM/compile"
+  )
+}
